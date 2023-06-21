7ef082a20bd0b665e875d494f914a6bae033a631
DevBhojani116 <devbhojani116@gmail.com>
2023-06-21 10:47:54 +0530

    debugged whatsapp message sender code

--- Whatsapp message sender.py
+++ Whatsapp message sender.py
@@ -1,24 +1,14 @@
 import pyautogui as pg
 import time
 
-# time.sleep(5)
-# for i in range(2):
-#     pg.write("Explaining PyAutoGUI... message no. ",str(i))
-#     pg.press("Enter")
-
+time.sleep(5)
+for i in range(2):
+    pg.write("Explaining PyAutoGUI... message no. " + str(i+1))
+    pg.press("Enter")
+ 
 
 print(pg.write("This is a text written slowly",interval=0.2))
 pg.press("Enter")
-    
-
-
-
-
-
-
-
-
-
 
 
 
